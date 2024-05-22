a7a509d7842107deca48be3971eddf4e3c62040b
imgurbot12 <imgurbot12@gmail.com>
2024-05-21 21:00:22 -0700

    feat: makefile and better shader/background getters

--- src/main.rs
+++ src/main.rs
@@ -10,9 +10,9 @@ mod event;
 mod graphics;
 mod lock;
 
-//TODO: is disolve supposed to flicker like that?
-
 use crate::config::Settings;
+
+const XDG_PREFIX: &'static str = "dynlock";
 
 #[derive(Debug, Clone, Parser)]
 struct Cli {
@@ -26,7 +26,7 @@ struct Cli {
 
 impl Cli {
     /// Attempt to Find Valid Filepath of Certain Extension
-    fn find_file(p: &PathBuf, ext: &str) -> Result<PathBuf> {
+    fn find_file(p: &PathBuf, extensions: Vec<&str>) -> Result<PathBuf> {
         if !p.exists() {
             return Err(anyhow!(format!("no such file: {p:?}")));
         }
@@ -34,27 +34,37 @@ impl Cli {
             let mut rng = rand::thread_rng();
             let files = std::fs::read_dir(&p)
                 .context(format!("failed to read dir: {p:?}"))?
-                .filter_map(|f| f.ok())
-                .filter(|f| f.path().ends_with(ext));
+                .filter_map(|e| e.ok())
+                .map(|e| e.path())
+                .filter(|f| {
+                    let ext = f
+                        .extension()
+                        .map(|s| s.to_string_lossy().to_string())
+                        .unwrap_or_default();
+                    extensions.contains(&ext.as_str())
+                });
             files
                 .choose(&mut rng)
-                .context(format!("no files present in {p:?}"))?
-                .path()
+                .context(format!("no files present in dir {p:?}"))?
         } else {
             p.to_owned()
         })
     }
     /// Prepare CLI Flags for Use and Generate Settings
     pub fn settings(mut self) -> Result<Settings> {
-        // attempt to Resolve Shader/Background FilePaths
-        let fragment = if let Some(shader) = self.shader.as_ref() {
-            Self::find_file(shader, ".glsl").context("failed to find shader")?
-        } else {
-            todo!()
-        };
+        // attempt to resolve shader/background filepaths
+        let fragpath = self.shader.clone().unwrap_or_else(|| {
+            xdg::BaseDirectories::with_prefix(XDG_PREFIX)
+                .expect("failed to read xdg base-dirs")
+                .get_config_file("shaders")
+        });
+        let fragment = Self::find_file(&fragpath, vec!["glsl"]).context("failed to find shader")?;
+        log::info!("loading fragment shader: {fragment:?}");
         if let Some(background) = self.background.as_ref() {
-            self.shader =
-                Some(Self::find_file(background, "").context("failed to find background")?);
+            let bpath = Self::find_file(background, vec!["png", "jpg", "jpeg"])
+                .context("failed to find background")?;
+            log::info!("loading background image: {bpath:?}");
+            self.background = Some(bpath);
         }
         // load Shader from File (if present)
         let shader = std::fs::read_to_string(fragment).context("failed to read shader file")?;
